e919028b3de9e6a3fd88a83aeacbcacdfbbbda1d
Youwithouto <youwithouto.z@gmail.com>
2021-01-21 22:36:49 +1100

    feat: Add week08/2021-01-21

---
+++ week08/2021-01-21/bloomfilter.py
@@ -0,0 +1,26 @@
+from bitarray import bitarray
+import mmh3
+
+# hash_num 表示每一个元素会对应多少个二进制位
+
+
+class BloomFilter:
+    def __init__(self, size, hash_num) -> None:
+        super().__init__()
+
+        self.size = size
+        self.hash_num = hash_num
+        self.bit_array = bitarray(size)
+        self.bit_array.setall(0)
+
+    def add(self, s):
+        for seed in range(self.hash_num):
+            result = mmh3.hash(s, seed) % self.size
+            self.bit_array[result] = 1
+
+    def lookup(self, s):
+        for seed in range(self.hash_num):
+            result = mmh3.hash(s, seed) % self.size
+            if self.bit_array[result] == 0:
+                return "Nope"
+        return "Probably"
